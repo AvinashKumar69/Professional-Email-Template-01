fc9ac4cfedbc299f9b5da4828cacc99f0ec34104
Avinash Kumar <ak2449746@gmail.com>
2022-05-23 06:33:03 +0530

    add Button-Css-Effect...

--- src/authentication/NewLoginForm.js
+++ src/authentication/NewLoginForm.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/NewLoginForm.css';
 
 const NewLoginForm = () => {
     return (
-        <div className=''>
-            <ul>
-                <li>
-                    <a href='#'>
-                        <span>Like</span>
-                    </a>
+        <div className='newLoginForm-body h-screen'>
+            <ul className='newLoginForm-ul'>
+                <li className='newLoginForm-li'>
+                    <Link className='newLoginForm-link' to='#'>
+                        <i className="fa-brands fa-google newLoginForm-i" />
+                        <span className='newLoginForm-span'>Login with Google</span>
+                    </Link>
+                </li>
+                <li className='newLoginForm-li'>
+                    <Link className='newLoginForm-link' to='#'>
+                        <i className="fa-brands fa-facebook newLoginForm-i" />
+                        <span className='newLoginForm-span'>Login with Facebook</span>
+                    </Link>
+                </li>
+                <li className='newLoginForm-li'>
+                    <Link className='newLoginForm-link' to='#'>
+                        <i className="fa-brands fa-github newLoginForm-i" />
+                        <span className='newLoginForm-span'>Login with GitHub</span>
+                    </Link>
+                </li>
+                <li className='newLoginForm-li'>
+                    <Link className='newLoginForm-link' to='#'>
+                        <i className="fa-brands fa-twitter newLoginForm-i" />
+                        <span className='newLoginForm-span'>Login with Twitter</span>
+                    </Link>
                 </li>
             </ul>
         </div>
