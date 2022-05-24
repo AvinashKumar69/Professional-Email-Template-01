8fbd8d49ac207f52a98cc2b7fe43e89fd9704693
Avinash Kumar <ak2449746@gmail.com>
2022-05-24 23:59:15 +0530

    ...

--- src/authentication/NewLoginForm.js
+++ src/authentication/NewLoginForm.js
@@ -1,33 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import '../styles/NewLoginForm.css';
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth, provider } from '../config/firebase-config';
+import { AuthContext } from '../context/AuthContext';
+import { useToast } from '@chakra-ui/react';
+
+
 
 const NewLoginForm = () => {
+
+    const navigate = useNavigate()
+    const { dispatch } = useContext(AuthContext)
+    const toast = useToast()
+
+    const handleGoogleLogin = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // ...
+                console.log('user of loginWithGoogle-->', user);
+                dispatch({ type: 'LOGIN', payload: user })
+
+                toast({
+                    title: 'LoggedIn!',
+                    description: "Your Successfully Loggedin via Google!!",
+                    status: 'success',
+                    duration: 5000,
+                    isClosable: true,
+                })
+
+                navigate('/contactus')
+
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+                console.log(errorMessage, email, credential, errorCode);
+
+                toast({
+                    title: 'Error, Something Went Wrong!',
+                    description: "Please Try Again!!",
+                    status: 'error',
+                    duration: 5000,
+                    isClosable: true,
+                })
+
+            });
+    }
+
     return (
         <div className='newLoginForm-body h-screen'>
             <ul className='newLoginForm-ul'>
                 <li className='newLoginForm-li'>
-                    <Link className='newLoginForm-link' to='#'>
+                    <Link className='newLoginForm-link' to='#'
+                        onClick={handleGoogleLogin}
+                    >
                         <i className="fa-brands fa-google newLoginForm-i" />
                         <span className='newLoginForm-span'>Login with Google</span>
-                    </Link>
-                </li>
-                <li className='newLoginForm-li'>
-                    <Link className='newLoginForm-link' to='#'>
-                        <i className="fa-brands fa-facebook newLoginForm-i" />
-                        <span className='newLoginForm-span'>Login with Facebook</span>
-                    </Link>
-                </li>
-                <li className='newLoginForm-li'>
-                    <Link className='newLoginForm-link' to='#'>
-                        <i className="fa-brands fa-github newLoginForm-i" />
-                        <span className='newLoginForm-span'>Login with GitHub</span>
-                    </Link>
-                </li>
-                <li className='newLoginForm-li'>
-                    <Link className='newLoginForm-link' to='#'>
-                        <i className="fa-brands fa-twitter newLoginForm-i" />
-                        <span className='newLoginForm-span'>Login with Twitter</span>
                     </Link>
                 </li>
             </ul>
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,11 +1,52 @@
-import React from 'react';
+// import React, { useRef } from 'react';
+// import emailjs from 'emailjs';
 
-const ContactUs = () => {
-    return (
-        <div className=''>
-            ContactUs
-        </div>
-    )
-}
+// const ContactUs = () => {
 
-export default ContactUs;
+//     const form = useRef();
+
+//     const sendEmail = (e) => {
+//         e.preventDefault();
+
+//         emailjs.sendForm('service_2epned2', 'template_46wdqow', form.current, 'ivLw-nnOrWGcfAsTS')
+//             .then((result) => {
+//                 console.log(result.text);
+//             }, (error) => {
+//                 console.log(error.text);
+//             });
+//         form.current.reset()
+//     };
+
+//     return (
+//         <div className='h-screen'
+//             style={{ background: '#434750' }}
+//             ref={form} onSubmit={sendEmail}
+//         >
+//             <div className='w-1/2 bg-white border-2 border-gray-300 sm:w-2/3 xs:w-full mx-auto'>
+//                 <form className=''>
+//                     <div className=''>
+//                         <label className=''>Name</label>
+//                         <input className='' name='name' type='text' placeholder='Enter Your Name' required />
+//                     </div>
+//                     <div className=''>
+//                         <label className=''>Phone Number</label>
+//                         <input className='' name='number' type='tel' placeholder='Enter Your Phone Number' required />
+//                     </div>
+//                     <div className=''>
+//                         <label className=''>Email Id</label>
+//                         <input className='' name='email' type='email' placeholder='Enter Your Email Id' required />
+//                     </div>
+//                     <div className=''>
+//                         <label className=''>Email Id</label>
+//                         <textarea className='' name='message' type='message' placeholder='Enter Your Message/ Feedback' rows='3' required />
+//                     </div>
+//                     <button type='submit' className=''>
+//                         Submit
+//                     </button>
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default ContactUs;
